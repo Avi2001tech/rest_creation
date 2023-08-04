8661db4455620127f499b7ce3bb6774408175441
Abhishek Kumar Shaw <avitech2001@gmail.com>
2023-08-04 22:45:09 +0530

    converting from sqlite to mongodb as database

--- app.js
+++ app.js
@@ -1,170 +1,128 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000; // You can change this to any desired port number
-
+const port = 3000;
 
 app.use(bodyParser.json());
 
-
-const db = new sqlite3.Database('database.db');
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS contacts (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phoneNumber TEXT NOT NULL UNIQUE
-  );
-`);
-
-db.run(`
-  CREATE TABLE IF NOT EXISTS spam_numbers (
-    id INTEGER PRIMARY KEY,
-    phoneNumber TEXT NOT NULL UNIQUE
-  );
-`);
-
-
-
-app.post('/contacts', (req, res) => {
-  const { name, email, phoneNumber } = req.body;
-
-  if (!name || !email || !phoneNumber) {
-    return res.status(400).json({ error: 'Name, email, and phone number are required.' });
-  }
-
-  const insertQuery = `
-    INSERT INTO contacts (name, email, phoneNumber)
-    VALUES (?, ?, ?);
-  `;
-
-  db.run(insertQuery, [name, email, phoneNumber], function (err) {
-    if (err) {
-      return res.status(500).json({ error: 'Failed to add the contact.' });
-    }
-
-    res.json({ id: this.lastID, name, email, phoneNumber });
-  });
+// Connect to MongoDB (replace 'your_mongodb_url' with your MongoDB connection string)
+mongoose.connect('mongodb://localhost:27017/your_database_name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+// Create a user schema and model
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-app.get('/contacts', (req, res) => {
-    const selectQuery = `
-      SELECT c.id, c.name, c.email, c.phoneNumber, s.id AS spamId
-      FROM contacts AS c
-      LEFT JOIN spam_numbers AS s ON c.phoneNumber = s.phoneNumber;
-    `;
-  
-    db.all(selectQuery, [], (err, rows) => {
-      if (err) {
-        return res.status(500).json({ error: 'Failed to fetch contacts.' });
-      }
-  
-      res.json(rows);
+const User = mongoose.model('User', userSchema);
+
+// ...
+
+// Secret key for JWT
+const secretKey = 'your_secret_key'; // Replace this with your secret key
+
+// Helper function to generate JWT token
+function generateToken(user) {
+  return jwt.sign(user, secretKey, { expiresIn: '1h' }); // Token expires in 1 hour
+}
+
+// POST endpoint for user registration
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: 'Name, email, and password are required.' });
+  }
+
+  try {
+    // Hash the password using bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name: name,
+      email: email,
+      password: hashedPassword,
     });
-  });
 
-  
+    await user.save();
 
-  app.get('/search/name/:query', (req, res) => {
-    const { query } = req.params;
-  
-    const searchQuery = `
-      SELECT c.id, c.name, c.email, c.phoneNumber, s.id AS spamId
-      FROM contacts AS c
-      LEFT JOIN spam_numbers AS s ON c.phoneNumber = s.phoneNumber
-      WHERE c.name LIKE ? || '%'
-      UNION
-      SELECT c.id, c.name, c.email, c.phoneNumber, s.id AS spamId
-      FROM contacts AS c
-      LEFT JOIN spam_numbers AS s ON c.phoneNumber = s.phoneNumber
-      WHERE c.name LIKE '%' || ? || '%'
-      AND c.name NOT LIKE ? || '%';
-    `;
-  
-    db.all(searchQuery, [query, query, query], (err, rows) => {
-      if (err) {
-        return res.status(500).json({ error: 'Failed to perform the search.' });
-      }
-  
-      res.json(rows);
-    });
-  });
-  
+    // Generate a JWT token for the registered user
+    const token = generateToken({ id: user._id, name: user.name, email: user.email });
 
+    res.json({ id: user._id, name: user.name, email: user.email, token });
+  } catch (err) {
+    res.status(500).json({ error: 'Error during registration.' });
+  }
+});
 
-  app.get('/search/phone/:query', (req, res) => {
-    const { query } = req.params;
-  
-    const searchQuery = `
-      SELECT c.id, c.name, c.email, c.phoneNumber, s.id AS spamId
-      FROM contacts AS c
-      LEFT JOIN spam_numbers AS s ON c.phoneNumber = s.phoneNumber
-      WHERE c.phoneNumber = ?;
-    `;
-  
-    db.all(searchQuery, [query], (err, rows) => {
-      if (err) {
-        return res.status(500).json({ error: 'Failed to perform the search.' });
-      }
-  
-      res.json(rows);
-    });
-  });
+// POST endpoint for user login
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-  
-  app.get('/contact/:id', (req, res) => {
-    const { id } = req.params;
-  
-    const selectQuery = `
-      SELECT c.id, c.name, c.email, c.phoneNumber, s.id AS spamId
-      FROM contacts AS c
-      LEFT JOIN spam_numbers AS s ON c.phoneNumber = s.phoneNumber
-      WHERE c.id = ?;
-    `;
-  
-    db.get(selectQuery, [id], (err, row) => {
-      if (err) {
-        return res.status(500).json({ error: 'Failed to fetch contact details.' });
-      }
-  
-      if (!row) {
-        return res.status(404).json({ error: 'Contact not found.' });
-      }
-  
-      res.json(row);
-    });
-  });
-  
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Email and password are required.' });
+  }
 
-app.post('/spam', (req, res) => {
-    const { phoneNumber } = req.body;
-  
-    if (!phoneNumber) {
-      return res.status(400).json({ error: 'Phone number is required.' });
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password.' });
     }
-  
-    
-    db.get(`SELECT * FROM spam_numbers WHERE phoneNumber = ?`, [phoneNumber], (err, row) => {
-      if (row) {
-        return res.status(400).json({ error: 'Number is already marked as spam.' });
-      }
-  
-      
-      db.run(`INSERT INTO spam_numbers (phoneNumber) VALUES (?)`, [phoneNumber], (err) => {
-        if (err) {
-          return res.status(500).json({ error: 'Failed to mark the number as spam.' });
-        }
-  
-        res.json({ message: 'Number marked as spam successfully.' });
-      });
-    });
+
+    // Compare the hashed password with the input password using bcrypt
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid email or password.' });
+    }
+
+    // Generate a JWT token for the logged-in user
+    const token = generateToken({ id: user._id, name: user.name, email: user.email });
+
+    res.json({ id: user._id, name: user.name, email: user.email, token });
+  } catch (err) {
+    res.status(500).json({ error: 'Error during login.' });
+  }
+});
+
+// Authentication middleware to check JWT token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized.' });
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Forbidden.' });
+    }
+
+    req.user = user;
+    next();
   });
-  
+}
+
+// Protected GET endpoint that requires authentication
+app.get('/protected', authenticateToken, (req, res) => {
+  const user = req.user;
+
+  res.json({ message: 'This is a protected endpoint.', user });
+});
+
+// ...
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
